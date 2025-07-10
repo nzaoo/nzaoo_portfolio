f9fd8c1ce1aeaf2275e7ef54c4dd3c55d61e8631
nzaoo <nzao1327@gmail.com>
2025-07-10 23:16:21 +0700

    update file

---
+++ traincar-demo/src/react-three-fiber.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    group: any;
+    mesh: any;
+    boxGeometry: any;
+    meshStandardMaterial: any;
+    ambientLight: any;
+    directionalLight: any;
+  }
+} 
